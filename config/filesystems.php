6314b2ca973c81112eeaf4c3a30253dbca07b72d
Herlambang Haryo <herlambangharyo@gmail.com>
2023-06-14 00:04:59 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,8 +69,13 @@ return [
     |
     */
 
+    // 'links' => [
+    //     public_path('storage') => storage_path('app/public'),
+    // ],
+
+
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        base_path('/public_html/storage') => storage_path('app/public'),
     ],
 
 ];
