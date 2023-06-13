8dd1f04e93ded05b392501652f339a29affa85c3
Herlambang Haryo <herlambangharyo@gmail.com>
2023-06-14 00:13:12 +0700

    restore

--- routes/web.php
+++ routes/web.php
@@ -46,12 +46,9 @@ use Illuminate\Support\Facades\Route;
 
     // --------------------------------------------------------------------
         Route::resource('AboutApp', AboutAppController::class);   
-    // --------------------------------------------------------------------
-        Route::get('/linkstorage', function () {
-            Artisan::call('storage:link');
-        });
+    // -------------------------------------------------------------------- 
 
-        
+
 Route::get('/', function () {
     return view('welcome');
 });
